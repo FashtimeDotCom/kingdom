495a16d35f45a5fee861ff8fa361f6fd21860ad9
Josue <josue.eduardo206@gmail.com>
2014-12-29 12:45:53 -0200

    Added ACcountControlTest

---
+++ src/test/java/com/josue/credential/manager/business/account/AccountControlTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josue.credential.manager.business.account;
+
+import com.josue.credential.manager.auth.credential.APICredential;
+import com.josue.credential.manager.auth.credential.APIDomainCredential;
+import com.josue.credential.manager.auth.manager.Manager;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+public class AccountControlTest {
+
+    @Mock
+    AccountRepository repository;
+
+    @Mock
+    Manager currentManager;
+
+    @InjectMocks
+    AccountControl control = new AccountControl();
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetManagerByCredential() {
+        //TODO testing nothing
+        String credUuid = "123";
+        Manager manager = mock(Manager.class);
+
+        when(repository.getManagerByCredential(credUuid)).thenReturn(manager);
+        Manager foundManager = control.getManagerByCredential(credUuid);
+        assertNotNull(manager);
+        assertEquals(manager, foundManager);
+    }
+
+    @Test
+    public void testGetAPICredentials() {
+        APIDomainCredential apiCredMock = mock(APIDomainCredential.class, Mockito.RETURNS_DEEP_STUBS);
+        List<APIDomainCredential> realList = Arrays.asList(apiCredMock, apiCredMock, apiCredMock);
+        when(repository.getApiCredentialsByManager(currentManager.getUuid())).thenReturn(realList);
+
+        APICredential credMock = mock(APICredential.class);
+        when(apiCredMock.getCredential()).thenReturn(credMock);
+        when(credMock.getApiKey()).thenReturn(UUID.randomUUID().toString());
+
+        List<APIDomainCredential> apiCredentials = control.getAPICredentials();
+        assertEquals(realList.size(), apiCredentials.size());
+        for (APIDomainCredential apicred : apiCredentials) {
+            assertNotNull(apicred);
+            
+            ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+            verify(credMock, times(3)).setApiKey(argument.capture());
+            assertTrue(argument.getValue().contains("*******"));
+        }
+    }
+
+    @Test
+    public void testInvite() {
+
+    }
+
+    @Test
+    public void testConfirm() {
+
+    }
+
+}
