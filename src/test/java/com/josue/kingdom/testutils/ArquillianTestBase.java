c1c36056714af236a574e6ea70225410a8c48e03
Josue <josue.eduardo206@gmail.com>
2015-05-15 22:07:22 +0100

    Added exception handling for ArquillianTestBase

--- src/test/java/com/josue/kingdom/testutils/ArquillianTestBase.java
+++ src/test/java/com/josue/kingdom/testutils/ArquillianTestBase.java
@@ -7,6 +7,7 @@ package com.josue.kingdom.testutils;
 
 import com.josue.kingdom.util.LiquibaseHelper;
 import java.io.File;
+import java.util.logging.Logger;
 import org.jboss.shrinkwrap.api.Filters;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -19,29 +20,36 @@ import static org.jboss.shrinkwrap.resolver.api.maven.Maven.resolver;
 public abstract class ArquillianTestBase {
 
     public static WebArchive createDefaultDeployment() {
+        WebArchive war = null;
+        try {
 
-        //        File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
-        File[] dependecies = resolver()
-                .loadPomFromFile("pom.xml")
-                .resolve("org.apache.shiro:shiro-core:1.2.3",
-                        "org.apache.shiro:shiro-web:1.2.3",
-                        "org.liquibase:liquibase-cdi:3.1.1",
-                        "com.sun.jersey:jersey-client:1.18.3") // ???... Container also needs the client dependencies
-                .withTransitivity().asFile();
+            //        File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
+            File[] dependecies = resolver()
+                    .loadPomFromFile("pom.xml")
+                    .resolve("org.apache.shiro:shiro-core:1.2.3",
+                            "org.apache.shiro:shiro-web:1.2.3",
+                            "org.liquibase:liquibase-cdi:3.1.1",
+                            "com.sun.jersey:jersey-client:1.18.3") // ???... Container also needs the client dependencies
+                    .withTransitivity().asFile();
 
-        WebArchive war = ShrinkWrap
-                .create(WebArchive.class, "kingdom-test.war")
-                .addPackages(true, Filters.exclude(LiquibaseHelper.class), "com.josue.kingdom")
-                .addClass(LiquibaseTestHelper.class)
-                .addAsResource("liquibase")
-                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                //                .addAsWebInfResource("test-ds.xml") //generic test DS
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/shiro.ini"))
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"))
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/jboss-deployment-structure.xml"))
-                .addAsLibraries(dependecies);
+            war = ShrinkWrap
+                    .create(WebArchive.class, "kingdom-test.war")
+                    .addPackages(true, Filters.exclude(LiquibaseHelper.class), "com.josue.kingdom")
+                    .addClass(LiquibaseTestHelper.class)
+                    .addAsResource("liquibase")
+                    .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                    //                .addAsWebInfResource("test-ds.xml") //generic test DS
+                    .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
+                    .addAsWebInfResource(new File("src/main/webapp/WEB-INF/shiro.ini"))
+                    .addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"))
+                    .addAsWebInfResource(new File("src/main/webapp/WEB-INF/jboss-deployment-structure.xml"))
+                    .addAsLibraries(dependecies);
+
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        }
 
         return war;
     }
+    private static final Logger logger = Logger.getLogger(ArquillianTestBase.class.getName());
 }
